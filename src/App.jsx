a40270549789e18962f693ae2eaa8889497f3f75
Michael Dear <michaelcdear@gmail.com>
2022-08-23 12:01:47 -0600

    front end work

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
     const movieElements = movieData.map(movie => {
         return (
-          <div className="movie-div" key={movie.id} >
+          <div className="movie-div" key={movie.id}>
               <img src={movie.thumbnail_url} key={movie.id} alt={""} className="movie-img" />
           </div>
         )
