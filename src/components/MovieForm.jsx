26fafec679d3da2ddf1da1bc894bdd0afc38b74c
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-08-24 13:25:59 -0600

    working on movie form

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 import '/Users/kelanhamman/Desktop/Development/code/phase-3/Phas3r-FrontEnd/src/components/stylesheets/movieform.css'
 
 function MovieForm() {
+
+    const[newMovie, setNewMovie] = useState({title: "", thumbnail_url: "", director: "",
+     description: "", budget: "", box_office: ""})
+
+      function handleSubmit(e) {
+        e.preventDefault();
+        fetch ('http://localhost:9292/movies', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: newMovie.title, thumbnail_url: newMovie.thumbnail_url, director: newMovie.director, 
+                description: newMovie.description, budget: newMovie.budget, box_office: newMovie.box_office
+
+            }),
+        })
+        setNewMovie()
+      }
+    
+  
   return (
     <div className="form">
+        <h1> Add a Movie </h1>
     <form>
       <div className="input-container">
         <label> Title </label>
@@ -26,6 +49,7 @@ function MovieForm() {
         <label> Box Office </label>
         <input type="text" name="Box Office" placeholder = "Box Office" />
       </div>
+      <button onSubmit={handleSubmit}> Add + </button>
     </form>
   </div>
   )
