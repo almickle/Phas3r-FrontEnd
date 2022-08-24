63a08cc17718dc2e3dac9a29cb30052f79fbe59c
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2022-08-24 11:41:54 -0600

    logo button goes HOME

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./stylesheets/navbar.css"
 
 function NavBar() {
 
+  let history = useHistory()
+
+  function handleClick(){
+    history.push("/")
+  }
+
   return (
     <div id="navbar">
-      <img id="navbar-logo" src="https://www.ohsu.edu/sites/default/files/2020-12/support240px.png" alt="" />
+      <img id="navbar-logo" src="https://www.ohsu.edu/sites/default/files/2020-12/support240px.png" alt="" 
+      onClick = {handleClick}/>
       <span id="banner">
           <Link className="route-link" to="/">Home</Link>
           <Link className="route-link" to="/movies">Movies</Link>
