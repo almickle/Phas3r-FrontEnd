c40ff5a932909089af86d54bbca59e9ab5ea4c13
Michael Dear <michaelcdear@gmail.com>
2022-08-24 10:47:40 -0600

    configured movie page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,8 +9,22 @@ function MovieCard({movie}) {
         <div className="movie-img-div">
             <img className="movie-img" src={movie.thumbnail_url} alt={""} />
         </div>
+        <div id="movie-info-div">
+            <h1 id="movie-title">{movie.title}</h1>
+            <li className="movie-info">Director: {movie.director}</li>
+            <li className="movie-info">Budget: ${movie.budget} million</li>
+            <li className="movie-info">Box Office: ${movie.box_office} million</li>
+            <div id="movie-desc-div">
+                <p id="movie-desc">{movie.description}</p>
+            </div>
+        </div>
+        {/* <div id="movie-desc-div">
+            <p id="movie-desc">{movie.description}</p>
+        </div> */}
     </div>
   )
 }
+
+
 
 export default MovieCard
