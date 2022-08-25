03cf0a0685a7eb183d29cc1525950785b921afec
Michael Dear <michaelcdear@gmail.com>
2022-08-25 13:36:07 -0600

    kjdkl

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -20,13 +20,13 @@ function Lightbulb({movie}) {
 
   const lightBulbs = []
   for(let i = 0; i < avgScore; i++) {
-    lightBulbs.push(<div><img src="https://th.bing.com/th/id/R.62464490bffe279897fb9742408400e6?rik=mo1QQF3kQ9Ubeg&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2facq%2feGK%2facqeGK8xi.jpeg&ehk=dIMEkEf%2f%2bLi%2fLj2dHIFLLmdGkNCIbrzMWUhhdnJxEwM%3d&risl=&pid=ImgRaw&r=0"/></div>)
+    lightBulbs.push(<div id="lightbulb-div"><img id="lightbulb-img" src="https://th.bing.com/th/id/R.62464490bffe279897fb9742408400e6?rik=mo1QQF3kQ9Ubeg&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2facq%2feGK%2facqeGK8xi.jpeg&ehk=dIMEkEf%2f%2bLi%2fLj2dHIFLLmdGkNCIbrzMWUhhdnJxEwM%3d&risl=&pid=ImgRaw&r=0"/></div>)
   }
 
 
   return (
 
-  <span>
+  <span id="lightbulb-span">
     {lightBulbs}
   </span>
 
