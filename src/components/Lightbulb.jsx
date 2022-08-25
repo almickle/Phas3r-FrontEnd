4b50d6224955fe693a123db2cbc5854ed5ebd86a
Michael Dear <michaelcdear@gmail.com>
2022-08-25 13:16:03 -0600

    lightbulbs

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -18,11 +18,16 @@ function Lightbulb({movie}) {
     console.log(avgScore)
   }, [avgScore])
 
+  const lightBulbs = []
+  for(let i = 0; i < avgScore; i++) {
+    lightBulbs.push(<div><img src="https://th.bing.com/th/id/R.62464490bffe279897fb9742408400e6?rik=mo1QQF3kQ9Ubeg&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2facq%2feGK%2facqeGK8xi.jpeg&ehk=dIMEkEf%2f%2bLi%2fLj2dHIFLLmdGkNCIbrzMWUhhdnJxEwM%3d&risl=&pid=ImgRaw&r=0"/></div>)
+  }
+
 
   return (
 
   <span>
-
+    {lightBulbs}
   </span>
 
   )
